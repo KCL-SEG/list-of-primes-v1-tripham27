0bd080fe5aef0e7b86f0166627a9c17fc2c9fdec
Tri Pham <K21003770@kcl.ac.uk>
2022-10-10 01:53:49 +0100

    completed task

--- primes.py
+++ primes.py
@@ -3,4 +3,13 @@
 
 def primes(number_of_primes):
     list = []
+    num = 2
+    while len(list) < number_of_primes:
+        prime = True
+        for x in range(1,num/2 + 1):
+            if num % x == 0:
+                prime = False
+        if prime:
+            list.append(num)
+        num += 1
     return list
